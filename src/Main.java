e4fc4685db8bbcbab3e36f5cfb104e09c827fe8f
NiK <nikrusk@list.ru>
2025-04-16 12:36:51 +0500

    Intial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    double ticket = 13_676;
+    int rub = 20;
+
+    int mile = (int) (ticket / rub);
+
+        System.out.println(mile);
+}
+}
